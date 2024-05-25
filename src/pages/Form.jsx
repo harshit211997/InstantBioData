598ef8db9640d2256ac2d87aea3cee776af2f3bb
Harshit Kumar <harshit211997@gmail.com>
2024-05-25 20:53:16 +0530

    Try to add actual biodata image as a preview behind the form

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -3,6 +3,7 @@ import '../App.css';
 import jsPDF from 'jspdf';
 import html2canvas from 'html2canvas';
 import image from "../assets/Fotos En Marcos Y Fondos Decorativos 702.jpeg";
+import background from "../assets/BioDataBackground.png";
 
 function BioDataForm() {
   const [fullName, setFullName] = useState('');
@@ -25,40 +26,43 @@ function BioDataForm() {
   };
 
   return (
-    <div className="container">
-      <h2>Bio Data Form</h2>
-      <form ref={formRef}>
-        <div className="form-group">
-          <label>Full Name:</label>
-          <input type="text" value={fullName} onChange={(e) => setFullName(e.target.value)} />
-        </div>
-        <div className="form-group">
-          <label>Age:</label>
-          <input type="number" value={age} onChange={(e) => setAge(e.target.value)} />
-        </div>
-        <div className="form-group">
-          <label>Gender:</label>
-          <select value={gender} onChange={(e) => setGender(e.target.value)}>
-            <option value="">Select</option>
-            <option value="male">Male</option>
-            <option value="female">Female</option>
-            <option value="other">Other</option>
-          </select>
-        </div>
-        <div className="form-group">
-          <label>Address:</label>
-          <input type="text" value={address} onChange={(e) => setAddress(e.target.value)} />
-        </div>
-        <div className="form-group">
-          <label>Email:</label>
-          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-        </div>
-        <div className="form-group">
-          <label>Phone Number:</label>
-          <input type="tel" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} />
-        </div>
-      </form>
-      <button type="button" onClick={handleDownloadPdf}>Download PDF</button>
+    <div className='container'>
+      <img className='bio-data' src={background}/>
+      <div>
+        <h2>Bio Data Form</h2>
+        <form ref={formRef}>
+          <div className="form-group">
+            <label>Full Name:</label>
+            <input type="text" value={fullName} onChange={(e) => setFullName(e.target.value)} />
+          </div>
+          <div className="form-group">
+            <label>Age:</label>
+            <input type="number" value={age} onChange={(e) => setAge(e.target.value)} />
+          </div>
+          <div className="form-group">
+            <label>Gender:</label>
+            <select value={gender} onChange={(e) => setGender(e.target.value)}>
+              <option value="">Select</option>
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+              <option value="other">Other</option>
+            </select>
+          </div>
+          <div className="form-group">
+            <label>Address:</label>
+            <input type="text" value={address} onChange={(e) => setAddress(e.target.value)} />
+          </div>
+          <div className="form-group">
+            <label>Email:</label>
+            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          </div>
+          <div className="form-group">
+            <label>Phone Number:</label>
+            <input type="tel" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} />
+          </div>
+        </form>
+        <button type="button" onClick={handleDownloadPdf}>Download PDF</button>
+      </div>
     </div>
   );
 }
